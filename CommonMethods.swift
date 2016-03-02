b5cff5656fbb1e05be41037367b82fa36deafc42
Sakshi Jain <Sakshi.jain@compassitesinc.com>
2016-03-02 15:22:02 +0530

    added attributed text method

--- CommonMethods.swift
+++ CommonMethods.swift
@@ -71,4 +71,47 @@ class CommonMethods: NSObject
         
         return finalLayer
     }
+    
+    
+    
+    
+    /* Atributted Text For BUTTON 
+       Things can be done: Custom Font Name and size,Color of text
+    */
+    
+    class func  AtributtedTitle (title: String ) -> NSMutableAttributedString
+    {
+        var finalStr : NSMutableAttributedString = NSMutableAttributedString ()
+        
+        let smallFont : UIFont = UIFont(name: "YOUR_FONTNAME", size: 14.0)!
+        let bigFont : UIFont = UIFont(name: "YOUR_FONTNAME", size: 15.0)!
+        
+        let small_textAttributes = [
+            NSForegroundColorAttributeName: "YOUR_FONT_COLOR_NAME",
+            NSFontAttributeName : smallFont
+        ]
+        let big_textAttributes = [
+            NSForegroundColorAttributeName: UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1.0),
+            NSFontAttributeName : bigFont
+        ]
+        
+        if title.lowercaseString.rangeOfString("YOUR STRING") != nil {
+            let range : Range = title.lowercaseString.rangeOfString("YOUR STRING")!
+            
+            if !(range.isEmpty)
+            {
+                let SubString : String = title.substringToIndex(range.startIndex)
+                let SubString1 : String = title.substringWithRange(range)
+                
+                finalStr.appendAttributedString(NSAttributedString(string: String (format: "    %@",SubString), attributes:big_textAttributes))
+                finalStr.appendAttributedString(NSAttributedString(string: String (format: " %@",SubString1), attributes:small_textAttributes))
+            }
+        }
+        else
+        {
+            finalStr.appendAttributedString(NSAttributedString(string: String (format: "    %@",title), attributes:big_textAttributes))
+        }
+        
+        return finalStr
+    }
 }
